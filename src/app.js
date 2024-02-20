7e2e5f1ea9feeeeb50a563c0dd5556a10a72cd68
Orianac0698 <150297944+Orianac0698@users.noreply.github.com>
2024-02-20 02:01:57 +0000

    message

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dom = [".com", ".edu", ".web"];
+
+  let dominio = function(pronoun, adj, noun, dom) {
+    let website = [];
+
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+          for (let l = 0; l < dom.length; l++) {
+            website.push(pronoun[i] + adj[j] + noun[k] + dom[l]);
+          }
+        }
+      }
+    }
+    console.log(website);
+  };
+
+  dominio(pronoun, adj, noun, dom);
 };
